77a02f05fb77b7862505fc430514c3ec4621bf9a
Eliyhu Chitrit <eliyhuc@college.labs>
2023-09-06 11:02:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 
-const port = 3001;
+const port = 3005;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
