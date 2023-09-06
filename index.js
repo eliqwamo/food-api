e08c3fb03685d3fd19d81eab292a112e54cfc2b5
Eliyhu Chitrit <eliyhuc@college.labs>
2023-09-06 10:59:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
